eb9c962adcc80c20e91d71720b175e5ab81dca3c
Abdullah Javed Nesar <abduljaved1994@gmail.com>
2017-06-07 20:16:51 +0530

    modified test format

--- utility_functions.py
+++ utility_functions.py
@@ -3,6 +3,9 @@ Utility functions for Constraints in Rubi
 '''
 import math
 
+from sympy.polys.polytools import degree
+from sympy.functions.elementary.hyperbolic import acosh
+
 def NonzeroQ(expr):
     return expr != 0
 
@@ -13,16 +16,16 @@ def ZeroQ(expr):
     return expr == 0
 
 def PositiveIntegerQ(var):
-    return isinstance(var, int) and var > 0
+    return var.is_Integer() and var > 0
 
 def NegativeIntegerQ(var):
-    return isinstance(var, int) and var < 0
+    return var.is_Integer() and var < 0
 
 def PositiveQ(var):
     return var > 0
 
 def IntegerQ(var):
-    return isinstance(var, int)
+    return var.is_Integer()
 
 def PosQ(var):
     return var > 0
@@ -39,27 +42,109 @@ def NegQ(var):
 def RationalQ(var):
     return var.is_rational_function()
 
-# todo
-def Subst(expr):
-    return 
+def Subst(a, x, y):
+    return a.subs(x, y)
 
-def linearQ(var, x):
-    return
-
-def IntLinearcQ(*args, x):
-    return
-
-def SumSimplerQ(expr, var):
-    return
-
-def SimplerQ(expr1, expr2):
-    return
+def linearQ(expr, x):
+    if degree(expr, gen=x) == 1:
+        return True
 
 def TogetherSimplify(expr):
+    return
+
+def Coefficient(u, var, n):
+    return 
+
+def RemoveContent():
+    return
+
+def Sqrt(a):
+    return math.sqrt(a)
+
+def ExpandIntegrand(expr, x):
+    return 
+
+def ArcCosh(a):
+    return acosh(a)
+
+def With():
+    return
+
+def Denominator():
     return
 
 def Hypergeometric2F1():
     return
 
-def Coefficient():
+def TogetherSimplify():
+    return
+
+def IntLinearcQ():
+    return
+
+def ArcTan():
+    return
+
+def Not():
+    return
+
+def Simplify():
+    return
+
+def FractionalPart():
+    return
+
+def IntegerPart():
+    return
+
+def Simp():
+    return
+
+def Rt():
+    return
+
+def Log():
+    return
+
+def SumSimplerQ():
+    return
+
+# utility functions used in tests
+def AppellF1():
+    return
+
+def Integrate():
+    return
+
+def hypergeom():
+    return
+
+def EllipticPi():
+    return
+
+def EllipticE():
+    return
+
+def EllipticF():
+    return
+
+def arctan():
+    return
+
+def arctanh():
+    return
+
+def arcsin():
+    return
+
+def arcsinh():
+    return
+
+def arccos():
+    return
+
+def arccosh():
+    return
+
+def arccsc():
     return
